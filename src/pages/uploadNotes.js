5af9127d31f6ba516409a711fe184b08f6e1d527
gunjan543 <gunjanagarwal54321@gmail.com>
2021-04-19 11:57:13 +0530

    Modified Form

--- src/pages/uploadNotes.js
+++ src/pages/uploadNotes.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React ,{ useState} from "react";
 import './uploadNotes.css';
 import $ from 'jquery';
+import { Subjects } from '../subjectsData';
 export default function UploadNotes() {
+	const [ semester, setSemester ] = useState('I');
+	const [ branch,setBranch ] = useState("");
+
+	function setValues1(e)
+	{
+      setSemester(e.target.value);
+	}
+
+	function setValues2(e)
+	{
+      setBranch(e.target.value);
+	}
+	
     $('.dropdown-el').click(function(e) {
         e.preventDefault();
         e.stopPropagation();
@@ -22,27 +36,123 @@ export default function UploadNotes() {
 						</p>
 						<p>
 								<label>Email: </label>
-								<input type="text" autocomplete="new-password"/>
+								<input type="email" autocomplete="new-password"/>
 						</p>
 						<p>
-								<label>Phone: </label>
-								<input type="tel" autocomplete="new-password"/>
-						</p>
-						<p>
-								<label>Subject: </label>
-                                <span class="select">
-  <select name="slct" id="slct">
-    <option selected disabled>Choose an option</option>
-    <option value="1" className="subject-name">Pure CSS</option>
-    <option value="2">No JS</option>
-    <option value="3">Nice!</option>
-  </select>
-</span>
+							<label>Role: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </label>
+							<span class="select">
+							<select name="slct" id="slct">
+								<option selected disabled>Choose an option</option>
+								<option value="1" >Teacher</option>
+								<option value="2" >Student</option>
+							</select>
+							</span>
     
-  			</p>
+  		            	</p>
 						<p>
-								<label>Message: </label>
-								<textarea></textarea>
+								<label>Class: &nbsp; &nbsp;&nbsp;</label>
+                                <span class="select">
+								<select name="slct" id="slct">
+									<option selected disabled>Choose an option</option>
+									<option value="1" >I year</option>
+									<option value="2" >II year</option>
+									<option value="3" >III year</option>
+									<option value="4" >IV year</option>
+								</select>
+								</span>
+    
+  			            </p>
+						<p>
+								<label>Semester: </label>
+                                <span class="select">
+								<select name="slct" id="slct" onChange={setValues1}>
+									<option selected disabled>Choose an option</option>
+									<option value="I" > I </option>
+									<option value="II" > II </option> 
+									<option value="III" > III </option>
+									<option value="IV" > IV </option>
+									<option value="V" > V </option>
+									<option value="VI" > VI </option>
+									<option value="VII" > VII </option>
+									<option value="VIII" > VIII </option>
+								</select>
+								</span>
+    
+  			            </p> 
+						<p>
+								<label>Branch:&nbsp;&nbsp; </label>
+                                <span class="select">
+								<select name="slct" id="slct" onChange={setValues2}>
+									<option selected disabled>Choose an option</option>
+									<option value="Computer Science" >Computer Science</option>
+									<option value="Electrical" >Electrical</option>
+									<option value="Civil" >Civil</option>
+									<option value="Electronics and Communication" >Electronics and Communication</option>
+								</select>
+								</span>
+    
+  			            </p>
+						<p>
+								<label>Year: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                <span class="select">
+								<select name="slct" id="slct">
+									<option selected disabled>Choose an option</option>
+									<option value="1" > 2021 </option>
+									<option value="2" > 2020 </option> 
+									<option value="3" > 2019 </option>
+									<option value="3" > 2018 </option>
+									<option value="3" > 2017 </option>
+									<option value="3" > 2016 </option>
+									<option value="3" > 2015 </option>
+									<option value="3" > 2014 </option>
+									<option value="3" > 2013 </option>
+									<option value="3" > 2012 </option>
+									<option value="3" > 2011 </option>
+									<option value="3" > 2010 </option>
+								</select>
+								</span>
+    
+  			            </p> 
+						<p>
+								<label>Type: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                                <span class="select">
+								<select name="slct" id="slct">
+									<option selected disabled>Choose an option</option>
+									<option value="1"> Notes </option>
+									<option value="2"> Important Questions </option>
+									<option value="3" > Question Papers </option>
+								</select>
+								</span>
+    
+  			            </p>  
+						  <p>
+								<label>Subject:&nbsp;&nbsp; </label>
+
+                                <span class="select">
+								<select name="slct" id="slct">
+									{
+									  Subjects.filter(semester => Subjects.semester).map(filteredSemester => (
+										Subjects.filter(branch => Subjects.branch).map(filteredBranch =>
+											(
+                           
+											)
+									  )) 	
+									}
+								<select name="slct" id="slct" onChange={setValues2}>
+									<option selected disabled>Choose an option</option>
+									<option value="Computer Science" >Computer Science</option>
+									<option value="Electrical" >Electrical</option>
+									<option value="Civil" >Civil</option>
+									<option value="Electronics and Communication" >Electronics and Communication</option>
+								</select>
+								</span>
+    
+  			            </p>
+						<p>
+								<label >Document: </label>
+								<span className="upload-file">
+								<input type="file" name="upload" accept=".xlsx,.xls,.doc, .docx,.ppt, .pptx,.txt,.pdf" className="subject-name"/>
+								</span>
 						</p>
 						<p class="wipeout">
 								<input type="submit" value="Send" />
---
+++ src/subjectsData.js
@@ -0,0 +1,215 @@
+export const Subjects =
+[
+ { 
+   "Semester" : "I",
+   "Branch" : "Computer Science" ,
+   "Subjects" :["Engineering mathematics -I",
+    "Engineering",
+    "Mathematics-II",
+    "Engineering Physics ",
+    "Engineering Chemistry",
+    "Communication Skills",
+    "Human Values",
+    "Programming for Problem Solving",
+    "Basic Mechanical Engineering"]
+ },
+ {
+    "Semester" : "II",
+    "Branch" : "Computer Science" ,
+    "Subjects" :["Engineering mathematics -I",
+        "Engineering",
+        "Mathematics-II",
+        "Engineering Physics ",
+        "Engineering Chemistry",
+        "Communication Skills",
+        "Human Values",
+        "Programming for Problem Solving",
+        "Basic Mechanical Engineering"]
+ },
+ {
+    "Semester" : "III",
+    "Branch" : "Computer Science" , 
+    "Subjects" :[
+        "Advanced Engineering Mathematics",
+        "Technical Communication/ Managerial Economics and Financial Accounting ",
+        "Digital Electronics ",
+        "Data Structures and Algorithms",
+        "Object Oriented Programming",
+        "Software Engineering" 
+    ]
+ },
+ {
+    "Semester" : "IV",
+    "Branch" : "Computer Science" , 
+    "Subjects" :[
+        "Discrete Mathematics Structure",
+        "Managerial Economicsand Financial Accounting",
+        "Technical Communication",
+        "Microprocessor & Interfaces",
+        "Database Management System",
+        "Theory of Computation",
+        "Data Communication and Computer Networks"
+    ]
+ },
+{
+    "Semester" : "V",
+    "Branch" : "Computer Science" , 
+    "Subjects" : [
+        "Information Theory & Coding",
+        "Compiler Design",
+        "Operating System",
+        "Computer Graphics & Multimedia",
+        "Analysis of Algorithms",
+        "Wireless Communication",
+        "Human-Computer Interaction",
+        "Bioinformatics"
+    ]
+},
+{
+    "Semester" : "VI",
+    "Branch" : "Computer Science" , 
+    "Subjects" :[
+        "Digital Image Processing",
+        "Machine Learning",
+        "Information Security System",
+        "Computer Architecture and Organization",
+        "Artificial Intelligence",
+        "Cloud Computing",
+        "Distributed System",
+        "Software Defined Network",
+        "Ecommerce and ERP"
+    ]
+},
+{
+    "Semester" : "VII",
+    "Branch" : "Computer Science" , 
+    "Subjects" :[
+       "Internet of Things",
+       "Cyber Security Lab"
+    ]
+},
+{
+    "Semester" : "VIII",
+    "Branch" : "Computer Science" , 
+    "Subjects" : [
+        "Big Data Analytics"
+    ]
+},
+{ 
+    "Semester" : "I",
+    "Branch" : "Electrical" ,
+    "Subjects" :["Engineering mathematics -I",
+     "Engineering",
+     "Mathematics-II",
+     "Engineering Physics ",
+     "Engineering Chemistry",
+     "Communication Skills",
+     "Human Values",
+     "Programming for Problem Solving",
+     "Basic Mechanical Engineering"]
+  },
+  {
+     "Semester" : "II",
+     "Branch" : "Electrical" ,
+     "Subjects" :["Engineering mathematics -I",
+         "Engineering",
+         "Mathematics-II",
+         "Engineering Physics ",
+         "Engineering Chemistry",
+         "Communication Skills",
+         "Human Values",
+         "Programming for Problem Solving",
+         "Basic Mechanical Engineering"]
+  },
+  { 
+    "Semester" : "I",
+    "Branch" : "Electronics and Communication" ,
+    "Subjects" :["Engineering mathematics -I",
+     "Engineering",
+     "Mathematics-II",
+     "Engineering Physics ",
+     "Engineering Chemistry",
+     "Communication Skills",
+     "Human Values",
+     "Programming for Problem Solving",
+     "Basic Mechanical Engineering"]
+  },
+  {
+     "Semester" : "II",
+     "Branch" : "Electronics and Communication" ,
+     "Subjects" :["Engineering mathematics -I",
+         "Engineering",
+         "Mathematics-II",
+         "Engineering Physics ",
+         "Engineering Chemistry",
+         "Communication Skills",
+         "Human Values",
+         "Programming for Problem Solving",
+         "Basic Mechanical Engineering"]
+  },
+  {
+    "Semester" : "III",
+    "Branch" : "Electronics and Communication" ,
+    "Subjects" :[
+        "Advanced Engineering Mathematics",
+        "Digital System Design",
+        "Signal & Systems",
+        "Network Theory ",
+        "Electronics Device"
+    ]
+  },
+  {
+    "Semester" : "IV",
+    "Branch" : "Electronics and Communication" ,
+    "Subjects" : [
+        "Advance Engineering Mathematics-II",
+        "Analog Circuits",
+        "Microcontrollers",
+        "Electronics Measurement & Instrumentation",
+        "Analog and Digital Communication"
+    ]
+  },
+  {
+    "Semester" : "V",
+    "Branch" : "Electronics and Communication" ,
+    "Subjects" :[
+        "Computer Architecture ",
+        "Electromagnetics Waves",
+        "Control system ",
+        "Digital Signal Processing",
+        "Microwave Theory & Techniques",
+        "Bio-Medical Electronics",
+        "Embedded Systems",
+        "Probability Theory & Stochastic Process",
+        "Satellite Communication"
+    ]
+  },
+  { 
+    "Semester" : "I",
+    "Branch" : "Civil" ,
+    "Subjects" :["Engineering mathematics -I",
+     "Engineering",
+     "Mathematics-II",
+     "Engineering Physics ",
+     "Engineering Chemistry",
+     "Communication Skills",
+     "Human Values",
+     "Programming for Problem Solving",
+     "Basic Mechanical Engineering"]
+  },
+  {
+     "Semester" : "II",
+     "Branch" : "Civil" ,
+     "Subjects" :["Engineering mathematics -I",
+         "Engineering",
+         "Mathematics-II",
+         "Engineering Physics ",
+         "Engineering Chemistry",
+         "Communication Skills",
+         "Human Values",
+         "Programming for Problem Solving",
+         "Basic Mechanical Engineering"]
+  }
+   ]
+
+
